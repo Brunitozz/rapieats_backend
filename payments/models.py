90e2f96dc225bb673f7e1fd080911229bacbe4ae
Bruno Pumapillo Sarmiento <122190257+Brunitozz@users.noreply.github.com>
2024-11-13 11:43:12 -0500

    Agregar modelo de Payment

--- payments/models.py
+++ payments/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
-# Create your models here.
 class Payment(models.Model):
     id_payment = models.AutoField(primary_key=True)
-    metod = models.CharField(max_length=100)
+    method = models.CharField(max_length=100) 
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     state = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.metodo} - {self.monto} ({self.estado})"
-     
+        return f"{self.method} - {self.amount} ({self.state})" 
