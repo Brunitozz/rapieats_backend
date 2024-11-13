524afd0ae071cb64126409af3f1a95cec88ff87a
Bruno Pumapillo Sarmiento <122190257+Brunitozz@users.noreply.github.com>
2024-11-13 10:56:05 -0500

    Agregar modelo de Payment con campos id, método, monto y estado

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Payment(models.Model):
+    id_payment = models.AutoField(primary_key=True)
+    metod = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    state = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.metodo} - {self.monto} ({self.estado})"
+     
